44175903559d382eea5369dcd61efe0a52339768
pushkarj122 <pushkar.jha.personal@gmail.com>
2024-12-11 21:15:37 +0530

    auth related work (login and register), and user model creation

---
+++ controllers/authController.js
@@ -0,0 +1,51 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: "User already exists." });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user = new User({
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully." });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Registration failed" });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials." });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid credentials." });
+    }
+
+    const token = jwt.sign({ userId: user._id }, "your_secret_key", {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Login failed." });
+  }
+};
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const bodyParser = require("body-parser");
 
 const app = express();
 connectDB();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", require("./routes/authRoutes"));
 // Sample route
 app.get("/", (req, res) => {
   res.send("Hello World!");
