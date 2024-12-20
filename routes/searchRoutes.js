0c90e9533a05a287e7907b9745b25e8556390e6d
pushkarj122 <pushkar.jha.personal@gmail.com>
2024-12-20 22:03:53 +0530

    adding admin based search

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const searchController = require("../controllers/searchController");
+const verifyTokenOptional = require("../middlewares/searchMiddleware");
 
 router.get("/", searchController.searchCentres); // For search
-// router.get("/:id", verifyToken, centreController.getCentreDetails); // For fetching center profile
+router.get("/:id", verifyTokenOptional, searchController.getCentreDetails); // For fetching center profile
 
 module.exports = router;
